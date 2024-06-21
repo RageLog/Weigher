287225103dca512242396d6a2806fe275a969d46
Ragelog <cemalltatli@gmail.com>
2024-06-21 21:02:09 +0300

    refactor: set Fusion style for WeigherUI application

--- Libraries/WeigherUI/WeigherUI.cpp
+++ Libraries/WeigherUI/WeigherUI.cpp
@@ -2,11 +2,14 @@
 
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQuickStyle>
 
 int WeigherUI::run(int argc, char* argv[])
 {
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
+    //NOTE: Next line is for style error
+    QQuickStyle::setStyle("Fusion");
     engine.load(QUrl(QStringLiteral("qrc:/UiComponent/main.qml")));
     return  app.exec();
 }
